98614ebda8d192b0cef6d6a8f0b8720e43db7766
shanuka55 <shanukalakshan5555@gmail.com>
2022-07-12 11:32:07 +0530

    project update

---
+++ ijse-Intake/src/controller/StudentFormcontroller.java
@@ -0,0 +1,30 @@
+package controller;
+
+import com.jfoenix.controls.*;
+import javafx.event.ActionEvent;
+
+public class StudentFormcontroller {
+    public JFXTextField txtStuentName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public JFXButton btnStudentTable;
+    public JFXButton btnDelete;
+    public JFXButton btnUpdate;
+    public JFXComboBox cmbStudentId;
+    public JFXButton btnAddStudent;
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtNIC;
+    
+
+    public void btnStudentTableOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+}
